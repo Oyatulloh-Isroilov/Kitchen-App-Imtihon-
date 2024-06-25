e3ef68addc8c3d56c981c406ede762e8db8118f1
Oyatulloh-Isroilov* <oyatullohisroilov5@gmail.com>
2024-06-25 19:57:35 +0600

    change 11

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,12 @@ function Home({ recipes, setRecipes }) {
                     title: 'Palov',
                     cookingTime: '45 daqiqa',
                     description: 'Palov juda mazali va tayyorlash oson bo`lgan ovqatdir. Palov O`zbekistonning milliy ovqatidir, tayyorlash uchun guruch, sabzi, go`sht, yog` masallig`lari kerak bo`ladi',
-                    ingredients: ['Guruch', 'Sabzi', 'Go`sht', 'Yog`'],
+                    ingredients: [
+                        { name: 'Guruch', cost: 5.99 },
+                        { name: 'Sabzi', cost: 3.49 },
+                        { name: 'Go`sht', cost: 12.99 },
+                        { name: 'Yog`', cost: 2.99 }
+                    ],
                     images: ['https://cdn.tasteatlas.com/images/dishes/27db9824904b49ec8e4164ce73bafb12.jpg?w=600', 'https://arbuz.com/wp-content/uploads/2009/12/Uzbek-Palov.jpg', 'https://sokin.moy.su/_ph/194/436166003.jpg'],
                     createdTime: new Date().toISOString()
                 },
@@ -27,7 +32,12 @@ function Home({ recipes, setRecipes }) {
                     title: 'Sho`rva',
                     cookingTime: '1.3,2soat',
                     description: 'Sho`rva, O`zbekistonda mashhur va sevimli bir ovqatdir. Bu taom odatda go`sht, sabzavotlar va unli naryalardan iborat. Sho`rva oshxonada ko`p variantlarda pishiriladi va har bir oilada o`ziga xos retsepti bo`ladi.',
-                    ingredients: ['Sabzi', 'Kartoshka', 'Yog', 'Go`sht',],
+                    ingredients: [
+                        { name: 'Sabzi', cost: 2.99 },
+                        { name: 'Kartoshka', cost: 1.99 },
+                        { name: 'Yog', cost: 1.49 },
+                        { name: 'Go`sht', cost: 9.99 }
+                    ],
                     images: ['https://kartinki.pics/uploads/posts/2022-12/thumbs/1670424713_5-kartinkin-net-p-shurpa-kartinki-vkontakte-5.jpg', 'https://ferganatourism.uz/thumb/2/v4SvIoQPtzzx7mlgX_H1gg/1200r1000/d/shurpa-3.jpg', 'https://zira.uz/wp-content/uploads/2020/08/kai--natma-shurpa-5-1.jpg'],
                     createdTime: new Date().toISOString()
                 },
@@ -36,7 +46,12 @@ function Home({ recipes, setRecipes }) {
                     title: 'Mastava',
                     cookingTime: '30-40 min',
                     description: 'Mastava, O`bekistonda mashhur va sevimli bir oziq-ovqat turi. Bu taom odatda bug`doy otalari (yengil bug`doy), un, suv va sabbalanib qo`shiladi. Mastava, o`sha miqdorda protein va qollaniladigan ma`zadagi ko`p xam butlar.',
-                    ingredients: ['Go`sht', 'Kartoshka', 'Yog`', 'Mosh'],
+                    ingredients: [
+                        { name: 'Go`sht', cost: 8.99 },
+                        { name: 'Kartoshka', cost: 1.99 },
+                        { name: 'Yog`', cost: 1.49 },
+                        { name: 'Mosh', cost: 0.99 }
+                    ],
                     images: ['https://kartinki.pics/uploads/posts/2021-03/1616215267_20-p-mastava-uzbekskii-sup-yeda-foto-24.jpg', 'https://ferganatourism.uz/thumb/2/v4SvIoQPtzzx7mlgX_H1gg/1200r1000/d/shurpa-3.jpg', 'https://zira.uz/wp-content/uploads/2020/08/kai--natma-shurpa-5-1.jpg', 'https://kartinki.pics/uploads/posts/2021-03/1616215239_15-p-mastava-uzbekskii-sup-yeda-foto-18.jpg'],
                     createdTime: new Date().toISOString()
                 }
